4e4d64fc8fa8137dc6c225e01f91cf1b3ed2e849
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-07-14 16:58:25 +0300

    fixing my local host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://my-defence-project.vercel.app/",
+    origin: ["http://localhost:5173", "https://my-defence-project.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
