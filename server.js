3cb1ac97dec14d18eb775680561e67ad32e59571
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-07-14 21:02:48 +0300

    fixed some upload issues

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static("backend/uploads"));
+app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working");
