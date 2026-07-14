014ab5c3b5eb49d1b56f354af5d29cd87b36b74b
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-07-14 16:39:39 +0300

    fixed cors

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://my-defence-project.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 // db connection //
 connectDB();
